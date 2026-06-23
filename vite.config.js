d68ce109cb9aaf741b11b4ac922777aa9fd53b4b
Alaya <ralaya@uiowa.edu>
2026-06-23 14:40:56 -0500

    moved mrms-index from static files to service

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     base: '/nexrad-l2/',
+    server: {
+        proxy: {
+            '/mrms-stats': {
+                target: 'https://visualriver.net',
+                changeOrigin: true,
+                secure: false,
+            }
+        }
+    }
 });
